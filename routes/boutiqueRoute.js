7b3c7ad7b6c8171044ec425b4524449e85fe2b7a
Floshooter <florent.bernier1@gmail.com>
2024-04-22 16:02:35 +0200

    REPAIR BOUTIQUEROUTE.JS

--- routes/boutiqueRoute.js
+++ routes/boutiqueRoute.js
@@ -3,13 +3,12 @@ const path = require('path');
 const multer = require('multer');
 const router = express.Router();
 const itemDAO = require('../controllers/itemDAO');
-// const middlewareAuth = require('../middleware/authorization')
-// const middlewareAdmin = require('../middleware/isAdmin');
+const middlewareAuth = require('../middleware/authorization')
+const middlewareAdmin = require('../middleware/isAdmin');
 
-router.use('/upload', express.static('products'));
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'upload/products');
+        cb(null, 'upload');
     },
     filename: (req, file, cb) => {
         console.log(file)
@@ -23,11 +22,11 @@ router.use(express.json());
 
 router.get('/', itemDAO.getAllItems);
 router.get('/id/:id',itemDAO.getItemById);
-router.post('/additem', upload.single('thumbnail'), itemDAO.addItem);
-router.get('/category', itemDAO.getCategory);
-router.put('/updatestocks/:id', itemDAO.updateStocks);
-router.get('/s', itemDAO.getItemByName);
-router.delete('/deleteitem/:id', itemDAO.deleteItem);
-router.put('/updateitem/:id', itemDAO.updateItem);
+router.post('/additem', middlewareAuth.authenticator, middlewareAdmin.isAdmin, upload.single('thumbnail'), itemDAO.addItem);
+router.get('/category', middlewareAuth.authenticator, itemDAO.getCategory);
+router.put('/updatestocks/:id', middlewareAuth.authenticator, middlewareAdmin.isAdmin, itemDAO.updateStocks);
+router.get('/s', middlewareAuth.authenticator,itemDAO.getItemByName);
+router.delete('/deleteitem/:id', middlewareAuth.authenticator, middlewareAdmin.isAdmin, itemDAO.deleteItem);
+router.put('/updateitem/:id', middlewareAuth.authenticator, middlewareAdmin.isAdmin, itemDAO.updateItem);
 
 module.exports = router;
